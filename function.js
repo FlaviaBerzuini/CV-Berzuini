e578632d4385491a62295ac6b2510fe225b9c124
FlaviaBerzuini <127367016+FlaviaBerzuini@users.noreply.github.com>
2023-03-08 23:34:29 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+function imprimircv(){
+    window.print("");
+}
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        document.querySelector('.go-top-container')
+        .classList.add('show');
+    }else{
+        document.querySelector('.go-top-container')
+        .classList.remove('show');
+    }
+}
+
+document.querySelector('go-top-container')
+.addEventListener('click', () =>{
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
